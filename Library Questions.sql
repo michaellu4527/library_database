06a25a2464d2ab58b9fb3af53592cdaa04dbb351
Michael Lu <mklu2005@yahoo.com>
2016-11-16 13:10:57 -0800

    Began polishing each query to make the results clearer

--- Library Questions.sql
+++ Library Questions.sql
@@ -24,7 +24,7 @@ where bl.CardNo IS NULL				/*Checks to see which entries of join are NOT in Book
 
 /*Question 4*/
 
-select * 
+select *
 from Library_Branch as lb INNER JOIN Book_Loans as bl
 ON lb.Branch_id = bl.Branch_id
 where bl.DueDate = '11/18/2016' AND lb.Branch_id = 1
@@ -53,4 +53,15 @@ select *
 from Book_Authors as ba INNER JOIN Book_Copies as bc
 ON ba.Book_id = bc.Book_id
 where ba.AuthorName = 'Stephen King' AND bc.Branch_id = 2
+
+
+/*Stored Procedure for alternate version of question 7*/
+alter proc GetBookAvailability @Author varchar(40), @Branch int
+AS
+	select * 
+	from Book_Authors as ba INNER JOIN Book_Copies as bc
+	ON ba.Book_id = bc.Book_id
+	where ba.AuthorName LIKE @Author + '%' AND bc.Branch_id = @Branch 
+
+EXEC GetBookAvailability 'm', 2
 
